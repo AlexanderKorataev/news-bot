a70178b191822aff3063dbcbefa0c55315069e7e
alexandr <a11@MacBook-Air-11.IGD_Rostelecom>
2024-09-24 00:16:04 +0700

    ~

--- src/handlers.py
+++ src/handlers.py
@@ -1,18 +1,15 @@
 from aiogram import Router, types
 from src.database import Database
+from src.config import MONGO_URI
 from aiogram import Bot, Dispatcher, html
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 
-# Bot token can be obtained via https://t.me/BotFather
-TOKEN = "7377998051:AAEYkjTJSrs373KrYO68Ni2I-Q9xbO1zuE0"
-
-# All handlers should be attached to the Router (or Dispatcher)
 
 router = Router()
-db = Database(mongo_uri='mongodb://mongo:27017')
+db = Database(mongo_uri=MONGO_URI)
 
 @router.message(CommandStart())
 async def start_command(message: types.Message):
