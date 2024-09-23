3fc1bee1515353b54deaaf3fb20e99e2c3ad3b69
alexandr <a11@MacBook-Air-11.IGD_Rostelecom>
2024-09-23 23:59:41 +0700

    ~

--- src/handlers.py
+++ src/handlers.py
@@ -1,20 +1,30 @@
 from aiogram import Router, types
 from src.database import Database
+from aiogram import Bot, Dispatcher, html
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+# Bot token can be obtained via https://t.me/BotFather
+TOKEN = "7377998051:AAEYkjTJSrs373KrYO68Ni2I-Q9xbO1zuE0"
+
+# All handlers should be attached to the Router (or Dispatcher)
 
 router = Router()
 db = Database(mongo_uri='mongodb://mongo:27017')
 
-@router.message(commands=["start"])
+@router.message(CommandStart())
 async def start_command(message: types.Message):
     await message.answer("Привет! Я новостной бот.")
 
-@router.message(commands=["addnews"])
+@router.message(Command("addnews"))
 async def add_news(message: types.Message):
     news = message.text[len("/addnews "):]
     await db.add_news({"news": news})
     await message.answer("Новость добавлена!")
 
-@router.message(commands=["getnews"])
+@router.message(Command("getnews"))
 async def get_news(message: types.Message):
     news_list = await db.get_news()
     response = "\n".join([item["news"] for item in news_list])
